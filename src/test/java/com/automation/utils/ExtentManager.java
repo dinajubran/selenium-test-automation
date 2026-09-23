03c02d028493cfdab5945a389803ffaf2aa775e1
dina jubran <dinajubran00@gmail.com>
2026-09-23 13:57:38 +0300

    add extent reports

---
+++ src/test/java/com/automation/utils/ExtentManager.java
@@ -0,0 +1,26 @@
+package com.automation.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentManager {
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/reports/index.html";
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+
+            spark.config().setReportName("Automation Test Results");
+            spark.config().setDocumentTitle("Test Execution Report");
+            spark.config().setTheme(Theme.STANDARD);
+
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+            extent.setSystemInfo("OS", System.getProperty("os.name"));
+            extent.setSystemInfo("Java Version", System.getProperty("java.version"));
+        }
+        return extent;
+    }
+}
---
+++ src/test/java/com/automation/utils/TestListener.java
@@ -0,0 +1,41 @@
+package com.automation.utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+    private static final ExtentReports extent = ExtentManager.getInstance();
+    private static final ThreadLocal<ExtentTest> testNode = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extent.createTest(result.getMethod().getMethodName());
+        testNode.set(test);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        testNode.get().log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        testNode.get().log(Status.FAIL, "Test Failed: " + result.getThrowable());
+        // To attach Base64 screenshot:
+        // testNode.get().addScreenCaptureFromBase64String(base64String, "Failure Screenshot");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        testNode.get().log(Status.SKIP, "Test Skipped: " + result.getThrowable());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush(); // Writes output to file
+    }
+}
